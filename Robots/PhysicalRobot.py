e5af818df8e51aaf639d8d93fc9861937981ce52
Junyao Shi <jackshi1999@gmail.com>
2019-10-17 08:56:10 +0100

    updated physicalrobot.py for positional servo, included adafruit servo kit

--- Robots/PhysicalRobot.py
+++ Robots/PhysicalRobot.py
@@ -1,5 +1,7 @@
 import math
 import serial
+from time import sleep
+from adafruit_servokit import ServoKit
 import time
 from math import cos, sin, pi
 import numpy as np
@@ -12,129 +14,113 @@ import matplotlib.pyplot as plt
 
 class PhysicalRobot(object):
 
-    def __init__(self, a1=0, a2=0, t_interval=125.0):
+    def __init__(self, a1=90, a2=90, a_upper=150, a_lower=30, delay=0.015):
         """
         :param a1: initial joint angle of proximal link
         :param a2: initial joint angle of distal link
+        :param a_upper: upper bound of joint angles
+        :param a_lower: lower bound of joint angles
         :param t_interval: time interval between each action
         """
-
-        # self.theta_displacement = 0
-        self.encoder_displacement = 0 # sum of the displacements detected by the two encoders
+        self.encoder_val = 0 # sum of the displacements detected by the two encoders
         self.a1 = a1
         self.a2 = a2
+        self.a_upper = a_upper
+        self.a_lower = a_lower
         self.a1dot = 0
         self.a2dot = 0
         self.state = (self.a1, self.a2)
 
-        # arduino controler
-        self.baudrate = 19200
-        self.arduino = serial.Serial('com11', self.baudrate)
+        # arduino and servo controller
+        self.kit = ServoKit(channels=16)
+        self.arduino = serial.Serial('/dev/ttyACM0', 9600)
 
         # constants
-        self.t_interval = t_interval
+        self.delay = delay
+    
+    def get_encoder(self):
+        self.arduino.write(str.encode('LE'))
+        left_encoder = int(self.arduino.readline())
+        self.arduino.write(str.encode('RE'))
+        right_encoder = int(self.arduino.readline())
+        return left_encoder, right_encoder
 
-    # accessor methods
-    def get_state(self):
-        return self.state
-    '''
-    # Transition Function Example For Physical Bot - Accepts action tuple (a1dot,a2dot) and fixed time interval t_interval 
-    # - returns joint angles and simple_reward using displacment from encoder values
     def move(self, action):
         a1dot, a2dot = action
+        print("action: ", action)
+        a1, a2 = self.a1, self.a2
+        a1_target = a1 + int(a1dot)
+        a2_target = a2 + int(a2dot)
+        
+        # enforce joint limits
+        if a1_target > self.a_upper or a1_target < self.a_lower:
+            a1_target = a1
+        if a2_target > self.a_upper or a2_target < self.a_lower:
+            a2_target = a2
+            
+        # ensure that current state is initial position
+        self.kit.servo[1].angle = a1 
+        self.kit.servo[2].angle = a2
+        
+        while a1 != a1_target and a2 != a2_target: 
+            if a1 < a1_target:
+                a1 += 1
+            elif a1 > a1_target:
+                a1 -= 1
+                
+            if a2 < a2_target:
+                a2 += 1
+            elif a2 > a2_target:
+                a2 -= 1
+                
+            # move joints accordingly
+            self.kit.servo[1].angle = a1
+            self.kit.servo[2].angle = a2
+            print(a1,a2, a1_target, a2_target)
+            sleep(self.delay) # time delay of 0.015 or 0.025 seems to work best
+      
+        assert a1 == a1_target and a2 == a2_target, "Problem with moving joint angles to target positions"
+        left_encoder,right_encoder = self.get_encoder() 
+        encoder_val = left_encoder + right_encoder
+        print('reward: {}'.format(encoder_val))
+        self.update_params(a1, a2, a1dot, a2dot, encoder_val)
+        return self.encoder_val, self.a1, self.a2
 
-        # SEND RATES OF CHANGE AND CONSTANT TIME INTERVAL = "Execution Time" to ARDUINO IDE via Serialport
-
-        self.arduino.write(chr(a1dot))  # send action
-        self.arduino.write(chr(a2dot))  # send action
-        time.sleep(self.t_interval)  # wait for action to finish then get new servo positioins
-
-        # GET SERVO ANGLES from Arduino IDE
-        self.arduino.write('pos1')  # Request servo position(s)
-        a1 = round(float(self.arduino.readline()), 2)  # Read serial port and save data
-        self.arduino.write('pos2')
-        a2 = round(float(self.arduino.readline()), 2)
-
-        # GET ENCODER VALUES from Arduino IDE
-        self.arduino.write('LE')  # Request Encoder value(s), NB they are returned here as the delta, not cumulative
-        Left_Encoder_Count = int(self.arduino.readline())
-        self.arduino.write('RE')
-        Right_Encoder_Count = int(self.arduino.readline())
-
-        # CALCULATE REWARD PARAMETER(S) FROM ENCODER VALUES
-        displacement = Left_Encoder_Count + Right_Encoder_Count  # Example of "simple reward" based on enocder values
-
-        self.update_alpha_dots(a2dot, a2dot)
-        self.update_params(a1, a2, displacement)
-
-        return self.state, self.encoder_displacement  # return new_state and displacment/reward
-    '''
-
-    # This funtion accepts "actions" as a tuple containing 3 variables (a1dot,a2dot,t_interval)
-    # can update which variables you want sent to the function
-    # make sure that a1dot and a2dot values range between (0-92-180)
-    # = (Full Speed CW - No Speed - Full Speed CCW), and any integers inbetween
-    # time interval range should stay between 125ms - 250ms
-    def move(self, action):
-        a1dot, a2dot = action
-        action_command = str(a1dot) + " " + str(a2dot) + " " + str(self.t_interval)
-        self.arduino.write(("start"))
-        validate = self.arduino.readline()
-        if len(validate) == len("ready") + 2:
-            self.arduino.write(action_command)
-            while True:
-                validate = self.arduino.readline()
-                if len(validate) == len("done") + 2:
-                    a1 = self.arduino.readline()
-                    a2 = self.arduino.readline()
-                    LE = self.arduino.readline()
-                    RE = self.arduino.readline()
-
-                    # calculate total encoder displacement
-                    displacement = LE + RE  # Example of "simple reward" based on enocder values
-
-                    self.update_alpha_dots(a1dot, a2dot)
-                    self.update_params(a1, a2, displacement)
-
-                    return self.encoder_displacement, self.a1, self.a2
-
-    # update the return portion however you would like currently return(servo1 angle, servo2 angle, Left Encoder count, Right encoder count)
-
-    def update_alpha_dots(self, a1dot, a2dot):
-        self.a1dot = a1dot
-        self.a2dot = a2dot
-
-    def update_params(self, a1, a2, displacement):
+    def update_params(self, a1, a2, a1dot, a2dot, encoder_val):
         self.a1 = a1
         self.a2 = a2
         self.state = (self.a1, self.a2)
-        self.encoder_displacement = displacement
+        self.encoder_dal = encoder_val
 
     def print_state(self):
         print('\nthe current state is: ' + str(self.state) + '\n')
 
 
 if __name__ == "__main__":
-    robot = PhysicalRobot(t_interval=125.0)
+    robot = PhysicalRobot()
 
     time = [0]
     a1 = [robot.a1]
     a2 = [robot.a2]
-    displacement = [robot.encoder_displacement]
+    displacement = [robot.encoder_val]
     print('initial a1: {} a2: {}'.format(robot.a1, robot.a2))
-    for t in range(100):
+    for t in range(6):
         print('Executing iteration number {}',format(t+1))
-        a1dot = 1/3 * cos(t/5)
-        a2dot = -1/3 * sin(t/5)
+        if t % 2 == 0:
+            a1dot = 30
+            a2dot = -30
+        else:
+            a1dot = -30
+            a2dot = 30
         action = [a1dot, a2dot]
         robot.move(action)
         print('action taken: {}'.format(action))
         print('robot a1: {} a2: {}'.format(robot.a1, robot.a2))
-        print('robot encoder displacement: {}'.format(robot.encoder_displacement))
+        print('robot encoder displacement: {}'.format(robot.encoder_val))
         time.append(t + 1)
         a1.append(robot.a1)
         a2.append(robot.a2)
-        displacement.append(robot.encoder_displacement)
+        displacement.append(robot.encoder_val)
 
     plt.plot(time, a1)
     plt.ylabel('a1 displacements')
