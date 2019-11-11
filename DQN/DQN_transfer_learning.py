8a1466df307a6fab88134c105dcf4ec8f2ccb99b
Jack Shi <jackshi1999@gmail.com>
2019-11-11 18:53:21 -0500

    updated transfer learning code

--- DQN/DQN_transfer_learning.py
+++ DQN/DQN_transfer_learning.py
@@ -1,37 +1,55 @@
-from DQN_swimming import *
-import os, sys
+import sys
+import json
 
 # Edit the system path as needed
 sys.path.append('/home/jackshi/DeepRobots')
 
-json_name = '/home/jackshi/DeepRobots/Trials/DQN_Swimming_Trial_16/30000 th episode model.json'
-h5_name = '/home/jackshi/DeepRobots/Trials/DQN_Swimming_Trial_16/30000 th episode weights.h5'
+from DQN.DQN_agent import DQN_Agent
+from Robots.PhysicalRobot import PhysicalRobot
+from utils.learning_helper import physical_forward_reward_function
+
+trial_dir = ''
+param_fname = ''
+json_fname = '/home/jackshi/DeepRobots/Trials/DQN_Swimming_Trial_16/30000 th episode model.json'
+h5_fname = '/home/jackshi/DeepRobots/Trials/DQN_Swimming_Trial_16/30000 th episode weights.h5'
+
+# load param file
+with open(param_fname) as param_file:
+    params = json.load(param_file)
 
 # transfer agent
-agent = DQNAgent(gamma=0.98, epsilon=1.0, epsilon_min=0.1, epsilon_decay=0.9999994,
-                 memory_size=20000, actions_params=(-pi/8, pi/8, pi/8), learning_rate=0.001)
-agent.load_model(json_name=json_name, h5_name=h5_name)
-
-# specify program information
-TIMESTAMP = str(datetime.datetime.now()).replace(' ', '_')[:-7]
-TRIAL_NAME = 'DQN_Swimming_Transfer_Random_Theta'
-TRIAL_NUM = 19
-PATH = 'Trials/' + TRIAL_NAME + '_Trial_' + str(TRIAL_NUM) + "_" + TIMESTAMP
-
-# create directory
-os.mkdir(PATH)
-os.chmod(PATH, 0o0777)
-
-# # Perform DQN
-# learning_results = perform_DQN(agent, episodes=2, iterations=100,
-#                                batch_size=8, C=200, randomize_theta=False, path=PATH)
-# agent, num_episodes, avg_rewards, std_rewards, avg_losses, std_losses = learning_results
-#
-# # Loss Plot
-# make_loss_plot(num_episodes, avg_losses, std_losses, path=PATH)
-#
-# # Learning Curve Plot
-# make_learning_plot(num_episodes, avg_rewards, std_rewards, path=PATH)
+robot = PhysicalRobot(params['delay'])
+a_lower = params['a_lower']
+a_upper = params['a_upper']
+a_interval = params['a_interval']
+action_params = (a_lower, a_upper, a_interval)
+episodes = params['episodes']
+iterations = params['iterations']
+total_iterations = episodes * iterations
+dqn_agent = DQN_Agent(robot=robot,
+                      reward_function=physical_forward_reward_function,
+                      trial_name="transfer learning test",
+                      trial_num=0,
+                      episodes=params['episodes'],
+                      iterations=params['iterations'],
+                      network_update_freq=params['network_update_freq'],
+                      check_singularity=False,
+                      is_physical_robot=True,
+                      input_dim=len(robot.state) + 2,
+                      output_dim=1,
+                      actions_params=action_params,
+                      model_architecture=params['model_architecture'],
+                      memory_size=total_iterations // 3,  # 10
+                      memory_buffer_coef=20,  # 20
+                      randomize_theta=False,
+                      batch_size=params['batch_size'],
+                      gamma=0.99,
+                      epsilon=1.0,
+                      epsilon_min=params['epsilon_min'],
+                      epsilon_decay=params['epsilon_decay'],
+                      learning_rate=params['learning_rate'],
+                      params=params)
+dqn_agent.load_model(json_name=json_fname, h5_name=h5_fname)
 
 # Policy Rollout
-policy_rollout(agent=agent, path=PATH)
+dqn_agent.policy_rollout(timesteps=50)
