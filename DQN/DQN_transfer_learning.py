b290ae1c9afbf849ff154c9b137cd2aa582dbb23
Junyao Shi <jackshi1999@gmail.com>
2020-05-29 15:51:51 -0400

    updated transfer learning code

--- DQN/DQN_transfer_learning.py
+++ DQN/DQN_transfer_learning.py
@@ -4,14 +4,14 @@ import json
 # Edit the system path as needed
 sys.path.append('/home/jackshi/DeepRobots')
 
-from DQN.DQN_agent import DQN_Agent
+from DQN.DQN_agent_v2 import DQN_Agent
 from Robots.PhysicalRobot import PhysicalRobot
 from utils.learning_helper import physical_forward_reward_function
 
-trial_dir = ''
-param_fname = ''
-json_fname = '/home/jackshi/DeepRobots/Trials/DQN_Swimming_Trial_16/30000 th episode model.json'
-h5_fname = '/home/jackshi/DeepRobots/Trials/DQN_Swimming_Trial_16/30000 th episode weights.h5'
+trial_dir = 'Trial_70_DQN_swimming_forward_12500_iters_updated_nn_2020-05-22-11-37'
+param_fname = trial_dir + 'params.json'
+model_json_fname = trial_dir + '50_th_episode_model.json'
+model_h5_fname = trial_dir + '50_th_episode_weights.h5'
 
 # load param file
 with open(param_fname) as param_file:
@@ -28,15 +28,13 @@ iterations = params['iterations']
 total_iterations = episodes * iterations
 dqn_agent = DQN_Agent(robot=robot,
                       reward_function=physical_forward_reward_function,
-                      trial_name="transfer learning test",
+                      trial_name="transfer_learning_test",
                       trial_num=0,
                       episodes=params['episodes'],
                       iterations=params['iterations'],
                       network_update_freq=params['network_update_freq'],
                       check_singularity=False,
                       is_physical_robot=True,
-                      input_dim=len(robot.state) + 2,
-                      output_dim=1,
                       actions_params=action_params,
                       model_architecture=params['model_architecture'],
                       memory_size=total_iterations // 3,  # 10
@@ -49,7 +47,7 @@ dqn_agent = DQN_Agent(robot=robot,
                       epsilon_decay=params['epsilon_decay'],
                       learning_rate=params['learning_rate'],
                       params=params)
-dqn_agent.load_model(json_name=json_fname, h5_name=h5_fname)
+dqn_agent.load_model(json_name=model_json_fname, h5_name=model_h5_fname)
 
 # Policy Rollout
 dqn_agent.policy_rollout(timesteps=50)
