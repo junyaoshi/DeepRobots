3ad41b7fcfca6a7fdeafa097629e964850fc987e
brandon <brandonwz6860@outlook.com>
2022-06-05 16:14:22 -0700

    bugfix + symmetry support

--- training_scripts/q_learn_wheelchair_theta_only.py
+++ training_scripts/q_learn_wheelchair_theta_only.py
@@ -43,12 +43,17 @@ def get_val_from_index(ind, low, high, n_bins):
 
 def get_action_from_index(ind, low, high, phidot_bins, psidot_bins):
 	phidot_ind = ind//phidot_bins
-	psidot_ind = ind - phidot_ind
+	psidot_ind = ind % phidot_bins
 
 	phidot_true = get_val_from_index(phidot_ind, low, high, phidot_bins)
 	psidot_true = get_val_from_index(psidot_ind, low, high, psidot_bins)
 
 	return phidot_true, psidot_true
+
+def get_phipsi_inds(action_ind, phidot_bins):
+	phidot_ind = action_ind//phidot_bins
+	psidot_ind =  action_ind % phidot_bins
+	return phidot_ind, psidot_ind
 
 def get_policy_score(q_table):
 	tot = 0
@@ -56,17 +61,20 @@ def get_policy_score(q_table):
 	for theta_ind in range(N_BINS):
 		theta = get_val_from_index(theta_ind, -pi, pi, N_BINS)
 
-		robot = WheelChairRobot(theta=theta)
+		robot = WheelChairRobot(theta=theta, t_interval = 1)
 		
-		action_ind = np.argmax(q_table[theta_ind, :])
-		phidot_true, psidot_true = get_action_from_index(action_ind, A_LOWER, A_UPPER, N_BINS, N_BINS)
-		action = (phidot_true, psidot_true)
+		curr_theta_ind = theta_ind
 		
-		robot.move(action)
+		for i in range(50):
+			action_ind = np.argmax(q_table[curr_theta_ind, :])
+			phidot_true, psidot_true = get_action_from_index(action_ind, A_LOWER, A_UPPER, N_BINS, N_BINS)
+			action = (phidot_true, psidot_true)
+			robot.move(action)
+			curr_theta_ind = convert_to_index(robot.theta, -pi, pi, N_BINS)
 
 		tot += robot.x
 
-	return tot
+	return tot/N_BINS
 
 def get_sym_score_theta(mat, theta_bins):
 	#NOTE: Right now assumes theta_bins is even
@@ -99,13 +107,34 @@ def e_greedy(eps, theta, q_table):
 	phidot_true, psidot_true = get_action_from_index(ind, A_LOWER, A_UPPER, N_BINS, N_BINS)
 	return ind, phidot_true, psidot_true
 
+
+def get_opp_theta_ind(theta_ind, n_bins):
+	opp_theta_ind = None
+	if(theta_ind == 0 or theta_ind == n_bins//2):
+		opp_theta_ind = theta_ind
+	else:
+		mid = n_bins//2
+		if(theta_ind > mid):
+			diff = theta_ind - mid
+			opp_theta_ind = mid - diff
+		else:
+			diff = mid - theta_ind
+			opp_theta_ind = mid + diff
+	
+	return opp_theta_ind
+
+
 #Assumes state is initialized as all 0
-def q_learn(q_table, robot, itr = 1000000, gamma = 0.85, eps = 0.8, alpha = 0.3):
+def q_learn(q_table, robot, itr = 1000000, gamma = 0.8, eps = 0.8, alpha = 0.9, use_sym = False):
 	#Initialize action (starting state assumed to be 0)
+	policy_scores = []
 	for i in range(itr):
 		if(i % 10000 == 0 and i != 0):
 			print("iteration:", i)
-			print(np.max(q_table))
+			print("policy score:", get_policy_score(q_table))
+
+		if(i % 500 == 0):
+			policy_scores.append(get_policy_score(q_table))
 		#Update s to current config (equivalent to s <- s' step)
 		curr_state = robot.state
 		#print(curr_state)
@@ -141,13 +170,26 @@ def q_learn(q_table, robot, itr = 1000000, gamma = 0.85, eps = 0.8, alpha = 0.3)
 		#Update Q-value
 		q_table[theta_ind, action_ind] = q_s_a + alpha * (reward + gamma * q_s_a_p - q_s_a)
 
-def locomote(robot, q_table, itr = 100, save_csv = True):
-	robot.reset()
-	curr_state = (0, 0, 0)
+		if use_sym:
+			opp_theta_ind = get_opp_theta_ind(theta_ind, N_BINS)
+			if opp_theta_ind != theta_ind:
+				phidot_ind, psidot_ind = get_phipsi_inds(action_ind, N_BINS)
+				#print(opp_theta_ind)
+				#print(theta_ind)
+				rev_action_ind = psidot_ind * N_BINS + phidot_ind
+				q_table[opp_theta_ind, rev_action_ind] = q_table[theta_ind, action_ind]
+
+	return policy_scores
+
+
+def locomote(robot, q_table, theta = 0, itr = 100, save_csv = True):
+	robot = WheelChairRobot(theta=theta, t_interval = 1)
+
+	curr_state = (theta, 0, 0)
 
 	x_pos = [0]
 	y_pos = [0]
-	thetas = [0]
+	thetas = [theta]
 	phis = [0]
 	psis = [0]
 	times = [0]
@@ -184,7 +226,7 @@ def locomote(robot, q_table, itr = 100, save_csv = True):
 		robot_params.append(robot_param)
 		
 	if(save_csv):
-		generate_csv(robot_params, "./results/wheelchair_results/" + "qlearn_result_test.csv")
+		generate_csv(robot_params, "./results/wheelchair_results/" + "qlearn_result_theta_3mil.csv")
 
 	return x_pos, y_pos, thetas, phis, psis, times
 
@@ -238,7 +280,7 @@ table = construct_table()
 init_score = get_policy_score(table)
 robot = WheelChairRobot(t_interval = 1)
 
-q_learn(table, robot)
+policy_scores = q_learn(table, robot, itr = 300000, use_sym = False)
 
 final_score = get_policy_score(table)
 
@@ -246,11 +288,18 @@ print("initial score", init_score)
 print("final score", final_score)
 
 
-x_pos, y_pos, thetas, phis, psis, times = locomote(robot, table, save_csv = False)
+x_pos, y_pos, thetas, phis, psis, times = locomote(robot, table, theta = 0, save_csv = False)
 
-#plot_trajectories(x_pos, y_pos, thetas, phis, psis, times)
+plot_trajectories(x_pos, y_pos, thetas, phis, psis, times)
 print(x_pos)
 plot_theta_q_vals(table)
+
+x_vals = 200 * np.arange(len(policy_scores))
+plt.plot(x_vals, policy_scores)
+plt.show()
+
+# for i in range(10):
+# 	print(get_val_from_index(i, -pi, pi, 10))
 
 
 
